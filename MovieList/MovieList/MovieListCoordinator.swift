d2c80d3ed5489601640eb6b81665d3684e363fd2
Steven Hill <stevenhill15@icloud.com>
2022-06-19 15:30:50 +0800

    Fetched images for each row in MovieListVC with async/await. Displayed the images in the cells.

--- MovieList/MovieList/MovieListCoordinator.swift
+++ MovieList/MovieList/MovieListCoordinator.swift
@@ -13,12 +13,11 @@ class MovieListCoordinator {
     let networkManager = NetworkManager()
 
     var movieNames: [String] = []
-    var movieImages: [UIImage] = []
+    var movieImages: [Int: UIImage] = [:]
     var movieController: MovieListVC!
 
     init(userQueryString: String) {
         self.userQueryString = userQueryString
-        networkManager.delegate = self
     }
 }
 
@@ -37,81 +36,15 @@ extension MovieListCoordinator: MovieListVCDelegate {
         }
     }
 
-    func fetchImageForMovie(at position: Int,
-                            completionHandler: @escaping (Swift.Result<UIImage, Error>) -> Void) {
-
-        let possibleLoadedImage = networkManager.getImage(at: position)
-        if let image = possibleLoadedImage {
-            completionHandler(.success(image))
-        } else {
-            let emptyImageError = NSError()
-            completionHandler(.failure(emptyImageError as Error))
-        }
-    }
-}
-
-extension MovieListCoordinator: NetworkManagerDelegate {
-
-    func updateMovies(movieSearch: MovieSearch) {
-//        networkManager.fetch(movieName: movieName) { Result in
-//            switch Result {
-//                case .success(let receivedMovieSearch):
-//                    DispatchQueue.main.async {
-//                        var names: [String] = []
-//                        for result in receivedMovieSearch.results {
-//                            names.append(result.trackName)
-//                        }
-//                        self.moviesArray = names
-//                        print("names: \(names)")
-//                        self.tableView.reloadData()
-//                    }
-//                case .failure(let error):
-//                    DispatchQueue.main.async {
-//                        let alertController = UIAlertController(title: "Something went wrong: \(error)", message: "Please try again.", preferredStyle: .alert)
-//                        let actionTitle = NSLocalizedString("Ok", comment: "")
-//                        alertController.addAction(.init(title: actionTitle, style: .default, handler: nil))
-//                        self.present(alertController, animated: true, completion: nil)
-//                        return
-//                    }
-//            }
-//        }
-    }
-
-    //    func updateMovies(movieSearch: MovieSearch) {
-    //        DispatchQueue.main.async {
-    //            var names: [String] = []
-    //            for result in movieSearch.results {
-    //                names.append(result.trackName)
-    //            }
-    //            if names.isEmpty {
-    //                let alertController = UIAlertController(title: "No movies found", message: "Please try another name.", preferredStyle: .alert)
-    //                let actionTitle = NSLocalizedString("Ok", comment: "")
-    //                alertController.addAction(.init(title: actionTitle, style: .default, handler: nil))
-    //                self.present(alertController, animated: true, completion: nil)
-    //                return
-    //            } else {
-    //            self.moviesArray = names
-    //            print("names: \(names)")
-    //            self.tableView.reloadData()
-    //            }
-    //        }
-    //    }
-
-    func movieSearchError(error: NetworkManager.FetchError) {
-//        DispatchQueue.main.async {
-//            let alertController = UIAlertController(title: "Something went wrong: \(NetworkManager.FetchError.self)", message: "Please try again.", preferredStyle: .alert)
-//            let actionTitle = NSLocalizedString("Ok", comment: "")
-//            alertController.addAction(.init(title: actionTitle, style: .default, handler: nil))
-//            self.present(alertController, animated: true, completion: nil)
-//            return
-//        }
-    }
-
-    func updateImages(images: [UIImage]) {
-//        DispatchQueue.main.async {
-//            self.movieImages = images
-//            self.tableView.reloadData()
-//            self.dismissLoadingView()
-//        }
+    func fetchImageForMovie(at position: Int) async throws -> UIImage {
+        return try await withCheckedThrowingContinuation({ continuation in
+            let possibleLoadedImage = networkManager.getImage(at: position)
+            if let image = possibleLoadedImage {
+                continuation.resume(returning: image)
+            } else {
+                let fetchError = NSError(domain: "Fetching Image", code: -1)
+                continuation.resume(throwing: fetchError)
+            }
+        })
     }
 }
